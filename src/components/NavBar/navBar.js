0103550501e4da9f819cdbc566c1baa0211cd9a1
Yash Gupta <yashgupta417@gmail.com>
2021-10-03 02:31:17 +0530

    small fix

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -7,7 +7,7 @@ function NavBar(props) {
     return (
         <div className="navbar-parent">
             <div><span className="logo-first">Code</span><span className="logo-second">Runner</span></div>
-            <Dropdown className="lang-dropdown" options={props.languages} value={props.languages[0]}
+            <Dropdown className="lang-dropdown" options={props.languages} value={"c++"}
                 controlClassName="lang-dropdown-control" onChange={props.languageChanged}
             />
             { !props.isRunning ? <button className="run-button" onClick={props.run}>Run</button> :
